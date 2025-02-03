25c4fe9de5d197f8c598cf71056c1a248d220d76
Shawn Ruby <snyrby@gmail.com>
2025-02-03 07:33:01 -0800

    feat: update new ticket component structure and styles, resolve merge conflicts

--- src/app/components/dashboard/support-dashboard/new-ticket/new-ticket.component.ts
+++ src/app/components/dashboard/support-dashboard/new-ticket/new-ticket.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
-<<<<<<< HEAD
 import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
   imports: [ButtonComponent, ControlComponent],
-=======
-
-@Component({
-  selector: 'app-new-ticket',
-  imports: [ButtonComponent],
->>>>>>> c4009a830215184469e4de863e7502573f085e6b
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
