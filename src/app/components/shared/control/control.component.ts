c387760063a0dda76fd5664dec0932add4547de4
Shawn Ruby <snyrby@gmail.com>
2025-02-04 07:33:33 -0800

    refactor: update dashboard components to use :host for styling and improve structure

--- src/app/components/shared/control/control.component.ts
+++ src/app/components/shared/control/control.component.ts
@@ -1,11 +1,35 @@
-import { Component, input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  // The following directive is responsible for using the styles as global
+  encapsulation: ViewEncapsulation.None,
+  // this will always add a property to the host element meaning every app-control will have the control class. This only applies if encapsulation is disabled
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
-  label = input.required<string>()
+  // this does the same thing as the host property in the component. This is considered discouraged.
+  // Within in the decorator, you can select the actual host property you want to change
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('click');
+  // }
+  label = input.required<string>();
+  private readonly el = inject(ElementRef);
+  onClick() {
+    console.log('click');
+    console.log(this.el);
+  }
 }
