f4f8dfe93d6b103aa924e0da5b376aba3db140b0
Shawn Ruby <snyrby@gmail.com>
2025-02-11 16:28:59 -0800

    feat: add ticket closing functionality and improve ticket status display

--- src/app/components/dashboard/support-dashboard/ticket/ticket.component.ts
+++ src/app/components/dashboard/support-dashboard/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { Ticket } from '../ticket.model';
 
 @Component({
@@ -9,9 +9,14 @@ import { Ticket } from '../ticket.model';
 })
 export class TicketComponent {
   data = input.required<Ticket>();
+  close = output();
   isDetailsVisible = signal<boolean>(false);
 
   onToggleDetails() {
     this.isDetailsVisible.set(!this.isDetailsVisible());
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit();
   }
 }
